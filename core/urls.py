a6716282a92778db0b44976606661a076d8bab89
Valteir <valteirluciano@gmail.com>
2021-05-16 08:51:39 -0300

    Logout, variavel timestamp do modelo product e inicio do tutorial com row e col

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home_page, about_page, contact_page, login_page, register_page
+from django.views.generic import TemplateView
+from .views import home_page, about_page, contact_page, login_page, logout_page, register_page
 
 
 urlpatterns = [
@@ -7,5 +8,7 @@ urlpatterns = [
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
     path('login/', login_page, name='login'),
+    path('logout/', logout_page, name='logout'),
     path('register/', register_page, name='register'),
+    path('bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
 ]
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,3 +125,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
+
+#LOGOUT_REDIRECT_URL = 'home_page'
